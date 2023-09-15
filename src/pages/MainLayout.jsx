d3c057e8eba7643129e08566d603f810677c3917
Cecilia <cecilia.sanchezlosada@gmail.com>
2023-09-15 12:41:38 +0200

    [Fix]: tickers animation

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -8,16 +8,11 @@ export default function MainLayout(props) {
     return (
         <div className="MainLayout">
             <main>
-                {/* MAIN first section */}
                 <div className="text-center main-container">
-                    {/* Content banner */}
                     <div className="row align-items-center mt-md-1">
                     </div>
-                    {/* MAIN second section */}
                     <div className='row d-flex align-middle justify-content-center mb-3 tickers'>
-                        <div className='col d-flex'>
-                            <Tickers tickers={tickers}/>
-                        </div>
+                        <Tickers tickers={tickers} />
                     </div>
                 </div>
             </main>
--- src/pages/Tickers.jsx
+++ src/pages/Tickers.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Tickers(props) {
   const { tickers } = props;
 
+  const root = document.documentElement;
+  const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+  const marqueeContent = document.querySelector("ul.marquee-content");
+
+  useEffect(() => {
+    if (marqueeContent !== null) {
+      root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+      for (let i = 0; i < marqueeElementsDisplayed; i++) {
+        marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true))
+      }
+    }
+  }, [marqueeContent])
+
   return (
     <div className='d-flex align-center'>
-      {Array.isArray(tickers) === true && tickers.map(item => {
-        return (
-        <div className='d-flex m-2 align-items-center' key={item.id}>
-          <img className="tickers-icon mx-1" src={item.icon} alt="" />
-          <div className='mx-1 text-white'>{item.name}</div>
-          <div className={item.last_value < item.current_value ? 'text-danger' : 'text-success'} style={{marginRight: 10, marginLeft: 10}}>{item.current_value.toFixed(2)}</div>
-          <div className='d-flex'>
-            <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{marginRight: 10}}>{Math.sign(item.variation) === 1 ? item.variation.toFixed(2) : item.variation.toFixed(4)}</div>
-            <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{marginRight: 10}}>{item.variation_percentage.toFixed(2) + '%'}</div>
-          </div>
-        </div>
-      )}
-      )}
+      <ul className='marquee-content'>
+        {Array.isArray(tickers) === true && tickers.map(item => {
+          return (
+            <li className='d-flex align-items-center' key={item.id}>
+              <img className="tickers-icon mx-1" src={item.icon} alt="" />
+              <div className='mx-1 text-white'>{item.name}</div>
+              <div className={item.last_value < item.current_value ? 'text-danger' : 'text-success'} style={{ marginRight: 10, marginLeft: 10 }}>{item.current_value.toFixed(2)}</div>
+              <div className='d-flex'>
+                <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{ marginRight: 10 }}>{Math.sign(item.variation) === 1 ? item.variation.toFixed(2) : item.variation.toFixed(4)}</div>
+                <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{ marginRight: 10 }}>{item.variation_percentage.toFixed(2) + '%'}</div>
+              </div>
+            </li>
+          )
+        }
+        )}
+      </ul>
     </div>
   )
 }
