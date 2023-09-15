31086c2c02beac7c5a5cfc89e1f9d88b0b94ef5c
Cecilia <cecilia.sanchezlosada@gmail.com>
2023-09-15 09:00:48 +0200

    [Style]: current value conditional style

--- src/pages/Tickers.jsx
+++ src/pages/Tickers.jsx
@@ -10,7 +10,7 @@ export default function Tickers(props) {
         <div className='d-flex m-2 align-items-center' key={item.id}>
           <img className="tickers-icon mx-1" src={item.icon} alt="" />
           <div className='mx-1 text-white'>{item.name}</div>
-          <div className='mx-2 text-white'>{item.last_value.toFixed(2)}</div>
+          <div className={item.last_value < item.current_value ? 'text-danger' : 'text-success'} style={{marginRight: 10, marginLeft: 10}}>{item.current_value.toFixed(2)}</div>
           <div className='d-flex'>
             <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{marginRight: 10}}>{Math.sign(item.variation) === 1 ? item.variation.toFixed(2) : item.variation.toFixed(4)}</div>
             <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{marginRight: 10}}>{item.variation_percentage.toFixed(2) + '%'}</div>
