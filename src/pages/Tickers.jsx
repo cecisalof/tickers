3323f25e243226e7f0762a1611804d71a07ffe78
Cecilia <cecilia.sanchezlosada@gmail.com>
2023-09-18 10:10:45 +0200

    [Fix]: signs and styles

--- src/pages/Tickers.jsx
+++ src/pages/Tickers.jsx
@@ -25,9 +25,13 @@ export default function Tickers(props) {
             <li className='d-flex align-items-center' key={item.id}>
               <img className="tickers-icon mx-2" src={item.icon} alt="" />
               <div className='mx-1 text-white'>{item.name}</div>
-              <div className={item.last_value < item.current_value ? 'text-danger' : 'text-success'} style={{ marginRight: 5, marginLeft: 5 }}>{item.name === 'EUR/USD' ? item.current_value.toFixed(4): item.current_value.toFixed(2)}</div>
-              <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{ marginRight: 5, marginLeft: 5 }}>{item.name === 'EUR/USD' ? item.variation.toFixed(4) : item.variation.toFixed(2)}</div>
-                <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{ marginRight: 5, marginLeft: 5 }}>{item.name === 'EUR/USD' ? item.variation_percentage.toFixed(4) + '%': item.variation_percentage.toFixed(2) + '%'}</div>
+              <div className={item.last_value < item.current_value ? 'text-danger' : item.last_value > item.current_value ? 'text-success' : 'text-light' } style={{ marginRight: 5, marginLeft: 5 }}>{item.name === 'EUR/USD' ? item.current_value.toFixed(4): item.current_value.toFixed(2)}</div>
+              <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{ marginRight: 5, marginLeft: 5 }}>
+                {Math.sign(item.variation) === 1 && item.name === 'EUR/USD' ? '+' + item.variation.toFixed(4) : Math.sign(item.variation) === 1 && !item.name !== 'EUR/USD' ? '+' + item.variation.toFixed(2) : Math.sign(item.variation) !== 1 && item.name === 'EUR/USD' ? item.variation.toFixed(4) : item.variation.toFixed(2)}
+                </div>
+                <div className={Math.sign(item.variation) === 1 ? 'text-success' : 'text-danger'} style={{ marginRight: 5, marginLeft: 5 }}>
+                  {Math.sign(item.variation_percentage) === 1 && item.name === 'EUR/USD' ? '( +' + item.variation_percentage.toFixed(3) + '%)' : Math.sign(item.variation_percentage) === 1 && !item.name !== 'EUR/USD' ? '( +' + item.variation_percentage.toFixed(2) + '%)' : Math.sign(item.variation_percentage) !== 1 && item.name == 'EUR/USD' ? '(' + item.variation_percentage.toFixed(3) + '%)' : '(' + item.variation_percentage.toFixed(2) + '%)'}
+                  </div>
             </li>
           )
         }
